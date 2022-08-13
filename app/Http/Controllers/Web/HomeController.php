5f9d07dc209fa27e09a119a71b6a70f9ffae718d
Ibrahim I Musabah <i.musabah92@gmail.com>
2022-08-13 12:56:40 +0800

    show all post in home page

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('frontend.index');
+        return view('frontend.index')->with([
+           'posts' => Post::paginate(10)
+        ]);
     }
 }
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,11 +1,33 @@
 <x-app-layout>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+        @forelse($posts as $post)
+            <div class="max-w-7xl w-6/12 mt-5 mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="pt-1 pl-4 bg-white border-b border-gray-200">
+                           <span class="text-xs"><a href="#" class="text-blue-600">{{$post->user->name}}</a> post</span>
+                           <p class="block text-xl pr-1"> {{$post->title}}</p>
+                    </div>
+                    <div class="p-4 bg-white text-base text-gray-500 border-b border-gray-200">
+                        {{$post->body}}
+                        <p class="pt-4 mt-1 text-xs">
+                            <span>Published since</span>
+                         {{$post->created_at->diffForHumans()}}
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
+        @empty
+             <div class="max-w-7xl mr-10 text-gray-500 my-32 mx-auto sm:px-6 lg:px-8">
+                 <div class="w-6/12 m-auto">
+                     Uhhhm,No data feed here,post now to be the first one
+                 </div>
+             </div>
+        @endforelse
+        @if($posts)
+
+            <div class="max-w-7xl w-6/12 mt-5 mx-auto sm:px-6 lg:px-8">
+                      {{$posts->links()}}
+            </div>
+         @endif
     </div>
 </x-app-layout>
