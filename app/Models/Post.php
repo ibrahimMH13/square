1685cd8e3312f86a8f0a047abdb2475f2e2ad303
Ibrahim I Musabah <i.musabah92@gmail.com>
2022-08-13 13:17:45 +0800

    add slug and change route key anme

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
+/**
+ * @property mixed title
+ * @property mixed|string slug
+ */
 class Post extends Model
 {
     use HasFactory;
@@ -14,6 +19,13 @@ class Post extends Model
        'body',
        'user_id',
     ];
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function (Post $post){
+            $post->slug = Str::slug($post->title);
+        });
+    }
 
     public function getRouteKeyName()
     {
